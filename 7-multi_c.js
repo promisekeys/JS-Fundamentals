87fa8f29b92d384aeb2cf0a6b9d65e4c173a1d7f
Ahamefula Promise <ahamefulapromise03@gmail.com>
2025-07-14 11:11:07 +0100

    Add 7-multi_c.js to print C is fun x times

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+// 7-multi_c.js
+
+const arg = parseInt(process.argv[2]);
+
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < arg; i++) {
+    console.log('C is fun');
+  }
+}
